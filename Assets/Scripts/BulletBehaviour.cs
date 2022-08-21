1ba1e98ba73f74a63af93072e885e9275f2e737d
groobob <91916133+groobob@users.noreply.github.com>
2022-08-21 09:38:31 -0400

    Added Growth Mechanic

--- Assets/Scripts/BulletBehaviour.cs
+++ Assets/Scripts/BulletBehaviour.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class BulletBehaviour : MonoBehaviour
 {
     [SerializeField] GameObject tree;
+    [SerializeField] GameObject leaves;
     bool nearGround = false;
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Ground" && nearGround == true)
         {
             Instantiate(tree, collision.contacts[0].point, Quaternion.identity);
+            Destroy(gameObject);
+        }
+        if(collision.gameObject.tag == "Growth")
+        {
+            Instantiate(leaves, collision.contacts[0].point, Quaternion.identity);
             Destroy(gameObject);
         }
     }
